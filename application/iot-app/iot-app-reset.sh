61ef4f8e89be7f9b9b9387ffb266adb53cc9df73
Sylvain Montagny <sylvain.montagny@univ-smb.fr>
2023-06-11 20:56:44 +0200

    Update path

--- application/iot-app/iot-app-reset.sh
+++ application/iot-app/iot-app-reset.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#Script to instanciate influxdb - telegraf - grafana 
-#Data are stored in folder iot-app
+
 path=/home/debian
 dest_folder=lorawan/application/iot-app
-
 
 function remove_file () {
 	rm -rf ${path}/${dest_folder}/influx_data/*
